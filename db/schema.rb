399c83086dee8945c67d2d5e5dd760feca12377a
t-techa2020 <t.techa2020@gmail.com>
2023-06-10 17:16:16 +0900

    feat: todos#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_074508) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_080856) do
+  create_table "todos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "user_name"
